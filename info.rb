5d0491da1fc41480f1fa31434baa191c442ff307
Balach Subra <balachsubra@gmail.com>
2017-02-24 17:35:40 +0000

    address book built

--- info.rb
+++ info.rb
@@ -1,16 +1,19 @@
 class Person 
-attr_accessor :dob, :first_name, :surname, :fullname, :phone_numbers
+  require 'date'
+attr_accessor :dob, :first_name, :surname, :fullname, :phone_numbers, :relationship
 attr_reader :emails, :phone_numbers
   
-  def initialize (first_name ='Joe', surname ='Bloggs', dob = "1 Jan 1990")
-    @dob = dob
-    @first_name = first_name
-    @surname = surname
-    @fullname = first_name + ' ' + surname
+  def initialize (first_name ="Joe", surname ="Bloggs", dob = "01-01-1990")
+    @dob = Date.parse(dob)
+    @first_name = first_name.capitalize
+    @surname = surname.capitalize
+    @fullname = @first_name + ' ' + @surname
     @emails = []  
     @phone_numbers = []
   end
-
+  def fullnum
+    
+  end
   def add_email(email)
     @emails.push(email)
   end
@@ -25,12 +28,12 @@ attr_reader :emails, :phone_numbers
   end
 
   def to_st
-      return "#{@fullname} was born on #{@dob}. Their email addresses are: #{@emails}.Their phone numbers are #{@phone_numbers}"
+      "#{@fullname} was born on #{@dob.strftime('%Y-%m-%d')}. Their email addresses are: #{@emails.to_s}.Their phone numbers are #{@phone_numbers.to_s}"
   end
 
   def print_details 
 
-    emailsStr = "";
+    emailsStr = ""
     @emails.each do |email|
       emailsStr += "- #{email} \n" 
     end
@@ -42,13 +45,98 @@ attr_reader :emails, :phone_numbers
 
     puts "#{@fullname}" 
     puts "\n"
-    puts  "--------------"
+    puts "-"
     puts "\n"
-    puts  "Date of Birth: #{@dob}"
+    puts  "Date of Birth: #{@dob.strftime('%-d %B %Y')}"
     puts "\n"
-    puts  "Email Addresses: #{emailsStr}"
-    puts  "Phone Numbers:\n #{phonesStr}"
-
-    
+    puts  "Email Addresses:\n#{emailsStr}"
+    puts  "Phone Numbers:\n#{phonesStr}" 
   end
 end
+
+class FamilyMember < Person
+  attr_accessor :relationship
+  
+  def initialiaze(first_name, surname, dob = nil, relationship = nil)
+    @relationship = relationship
+    super(first_name, surname, dob)
+  end
+
+  def add_email(email)
+    puts"#{email}"
+    super(email)
+  end
+
+def add_phone(phone)
+    puts"#{phone}"
+    super(phone_number)
+  end
+end
+
+class AddressBook < Person
+  attr_accessor :entries, :yaml
+  require 'yaml'
+  def initialize 
+    @entries = []
+  end
+
+  def add_p(person)
+    if person.is_a?Person
+      @entries.push(person)
+    else
+      raise "You must provide a valid Person object"
+    end
+  end
+
+   def add_email(email)
+    puts"#{email}"
+    super(email)
+  end
+
+def add_phone(phone)
+    puts"#{phone}"
+    super(phone_number)
+  end
+
+
+  def list
+    puts"Address Book"
+    puts"------------"
+    @entries.each_with_index do |person,i|puts "Entry #{i+1}: #{person.fullname}" 
+    end
+    return nil
+  end
+
+
+  def load_yaml(file_name)
+    yaml = YAML.load_file file_name
+    array = yaml["people"]
+    array.each do |yperson|
+      
+      if yperson ["relationship"]
+      person = FamilyMember.new yperson["fname"], yperson["surname"], yperson["dob"], yperson["relationship"]
+      else
+      person = Person.new yperson["fname"], yperson["surname"], yperson["dob"]  
+      end
+
+      yperson["emails"].each do |email|
+      person.add_email email
+      end
+
+      yperson["phones"].each do |phones|
+      person.add_phone_num   phones 
+      end
+      @entries << person
+      end 
+      return self
+    end
+  end   
+ 
+
+
+
+
+
+
+
+
--- spec/info_spec.rb
+++ spec/info_spec.rb
@@ -1,68 +1,80 @@
 
 describe Person do
+  let(:person){person = Person.new("joe", "bloggs", "1 Jan 1990") }
   it "Should Initialize" do
-    person = Person.new("joe", "bloggs", "1 Jan 1990")
-    expect(person.class).to eq Person
-    expect(person.dob).to eq "1 Jan 1990"
-    expect(person.first_name.capitalize).to eq "Joe"
-    expect(person.surname.capitalize).to eq "Bloggs"
-    #expect(person.fullname).to eq person.first_name, person.surname
-    expect(person.emails).to eq []
-    expect(person.phone_numbers).to eq []
+  
+  expect(person.dob.strftime('%-d %b %Y')).to eq "1 Jan 1990"
+  expect(person.first_name.capitalize).to eq "Joe"
+  expect(person.surname.capitalize).to eq "Bloggs"
+  expect(person.emails).to eq []
+  expect(person.phone_numbers).to eq []
   end
-  it "Should set dob" do
-    person = Person.new
-    person.dob = "1 Jan 1990"
-    expect(person.dob).to eq "1 Jan 1990"
+  it "Should take dob" do
+  person.dob = "01 jan 1990"
+  expect(person.dob).to eq "01 jan 1990"
   end
   it "Should take person First name" do
-    person = Person.new
-    person.first_name = "Joe"
-    expect(person.first_name).to eq "Joe"
+  
+  person.first_name = "Joe"
+  expect(person.first_name).to eq "Joe"
   end 
   it "Should take person Surname" do
-    person = Person.new
-    person.surname = "Bloggs"
-    expect(person.surname).to eq "Bloggs"
+  
+  person.surname = "Bloggs"
+  expect(person.surname).to eq "Bloggs"
   end
   it "Should display full name" do
-    person = Person.new
-    person.first_name = "Joe"
-    person.surname = "Bloggs"
-    expect(person.fullname).to eq "Joe Bloggs"
+  
+  person.first_name = "Joe"
+  person.surname = "Bloggs"
+  expect(person.fullname).to eq "Joe Bloggs"
   end
   it "Should take email address"do
-    person = Person.new 
-    person.add_email("joe@foo.com")
-    person.add_email("joe.bloggs@work.com")
-    expect(person.emails).to eq ["joe@foo.com","joe.bloggs@work.com"]
+   
+  person.add_email("joe@foo.com")
+  person.add_email("joe.bloggs@work.com")
+  expect(person.emails).to eq ["joe@foo.com","joe.bloggs@work.com"]
   end
   it "should take phone numbers"do
-    person = Person.new
-    person.add_phone_num("07712345678")
-    person.add_phone_num("02012345678")
-    expect(person.phone_numbers).to eq ["07712345678","02012345678"]
+  
+  person.add_phone_num("07712345678")
+  person.add_phone_num("02012345678")
+  expect(person.phone_numbers).to eq ["07712345678","02012345678"]
   end
   it "should delete a person's email" do
-    person = Person.new
-    person.add_email("joe@foo.com")
-    person.add_email("joe.bloggs@work.com")
-    expect(person.emails).to eq ["joe@foo.com","joe.bloggs@work.com"]
-    person.remove_email("joe@foo.com")
-    expect(person.emails).to eq ["joe.bloggs@work.com"]
+  
+  person.add_email("joe@foo.com")
+  person.add_email("joe.bloggs@work.com")
+  expect(person.emails).to eq ["joe@foo.com","joe.bloggs@work.com"]
+  person.remove_email("joe@foo.com")
+  expect(person.emails).to eq ["joe.bloggs@work.com"]
   end
   it "should show string form of person" do
-    person = Person.new("Joe", "Bloggs")
-    person.add_email("joe@foo.com")
-    person.add_email("joe.bloggs@work.com")
-    person.add_phone_num("07712345678")
-    person.add_phone_num("02012345678")
-    expect(person.emails).to eq ["joe@foo.com","joe.bloggs@work.com"]
-    person.remove_email("joe@foo.com")
-    expect(person.emails).to eq ["joe.bloggs@work.com"]
-    person.print_details()
-    expect(person.to_st).to eq ("Joe Bloggs was born on 1 Jan 1990. Their email addresses are: [joe.bloggs@work.com]. Their phone numbers are [07712345678,02012345678]")
-
+  person.add_email("joe@foo.com")
+  person.add_email("joe.bloggs@work.com")
+  person.add_phone_num("07712345678")
+  person.add_phone_num("02012345678")
+  expect(person.emails).to eq ["joe@foo.com","joe.bloggs@work.com"]
+  person.remove_email("joe@foo.com")
+  expect(person.emails).to eq ["joe.bloggs@work.com"]
+  expect(person.to_st).to eq ("Joe Bloggs was born on 1990-01-01. Their email addresses are: [\"joe.bloggs@work.com\"].Their phone numbers are [\"07712345678\", \"02012345678\"]")
+  person.print_details()
   end
 end
 
+  describe AddressBook do
+    let(:person1){person1 = Person.new("joe", "blogs", "1 Jan 1990")}
+    let(:person2){person2 = Person.new("andy", "nother", "2 Jan 1995")}
+    let(:book){book = AddressBook.new}
+    let(:entries){entries = AddressBook.new}
+    it "should Initialize"do
+    expect([].entries).to eq []
+  end
+  it "should add details to address book"do
+    book.add_p(person1)
+    book.add_p(person2)
+    expect(book.entries).to eq ([person1, person2])
+
+  end
+
+end
